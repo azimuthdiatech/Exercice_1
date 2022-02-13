9583d13b3d2a46ed8e7824fba8747e3f4bd0654f
azimuthdiatech <96420442+azimuthdiatech@users.noreply.github.com>
2022-02-13 20:01:41 +0000

    Update controller.php

--- exercice 17/controller.php
+++ exercice 17/controller.php
@@ -1,10 +1,3 @@
-<?php 
-include_once("fonctions.php");
-$jour=$_POST['jour'];
-$mois=$_POST['mois'];
-$annee=$_POST['annee'];
-dateValide($jour,$mois,$annee);
-dSuivant($jour,$mois,$annee);
-            
+<?php
 
 
