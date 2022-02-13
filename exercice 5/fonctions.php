ec222a86c85543c3dcfc7590c9a492a9a0cfc562
azimuthdiatech <96420442+azimuthdiatech@users.noreply.github.com>
2022-02-13 23:02:09 +0000

    Update fonctions.php

--- exercice 5/fonctions.php
+++ exercice 5/fonctions.php
@@ -1,5 +1,5 @@
 <?php 
-   //Procedures  "" ou ''=>chaine
+   //Procedures "" ou ''=>chaine
       function distance(int $xa,int $xb,int $ya,int $yb):void{
        $dist= sqrt(pow(($xb-$xa),2)+pow(($yb-$ya),2));
        echo "La distance entre x et y est ". $dist."<br>";
